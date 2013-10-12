9d01a8a2620ffcaa006f81e40c2c2d3528fb41c7
Wojnowski, Przemyslaw <przemyslaw.wojnowski@sabre.com>
2013-10-12 11:00:06 +0200

    Oracle XE env vars config.

---
+++ .oracle-xe-env.sh
@@ -0,0 +1,6 @@
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=${ORACLE_BASE}/product/11.2.0/xe
+export ORACLE_SID=XE
+export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+export PATH=$ORACLE_HOME/bin:$PATH
